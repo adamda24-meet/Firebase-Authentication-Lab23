9e0a25ce10755a791330b2c5aedabe514dcb3ff3
adamda24-meet <adamdarawshe380@gmail.com>
2023-07-23 18:35:15 +0300

    cool i did bounes

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,23 +2,64 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+config={
+     "apiKey": "AIzaSyDN08bQI3d0GV_-0Er3H4Od8CK_TB0ByPk",
+  "authDomain": "adamcs-c9c1a.firebaseapp.com",
+  "projectId": "adamcs-c9c1a",
+  "storageBucket": "adamcs-c9c1a.appspot.com",
+  "messagingSenderId": "876028938179",
+  "appId": "1:876028938179:web:f6980351553ab295a89f76",
+  "databaseURL":""
+}
+
+firebase=pyrebase.initialize_app(config)
+auth=firebase.auth()
+
+
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = "Authentication failed"
     return render_template("signin.html")
+
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = "Authentication failed"
     return render_template("signup.html")
+
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
     return render_template("add_tweet.html")
+
+@app.route('/signout')
+def signout():
+    login_session['user'] = None
+    auth.current_user = None
+    return redirect(url_for('signin'))
 
 
 if __name__ == '__main__':
