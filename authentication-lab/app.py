009fb2acab3df46bb75edddcbad586a2844b9132
adamda24-meet <adamdarawshe380@gmail.com>
2023-07-24 17:50:11 +0300

    i did it

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -9,11 +9,12 @@ config={
   "storageBucket": "adamcs-c9c1a.appspot.com",
   "messagingSenderId": "876028938179",
   "appId": "1:876028938179:web:f6980351553ab295a89f76",
-  "databaseURL":""
+  "databaseURL":"https://adamcs-c9c1a-default-rtdb.firebaseio.com/"
 }
 
 firebase=pyrebase.initialize_app(config)
 auth=firebase.auth()
+db = firebase.database()
 
 
 
@@ -42,24 +43,41 @@ def signup():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
+        full_name = request.form['full_name']
+        username = request.form['username']
+        bio = request.form['bio']
         try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            UID = login_session['user']['localId']
+            updated = {"full_name":full_name,"username": username ,"bio": bio,"email":email}
+            db.child("Users").child(UID).update(updated)
             return redirect(url_for('add_tweet'))
         except:
             error = "Authentication failed"
     return render_template("signup.html")
-
-
-
-@app.route('/add_tweet', methods=['GET', 'POST'])
-def add_tweet():
-    return render_template("add_tweet.html")
 
 @app.route('/signout')
 def signout():
     login_session['user'] = None
     auth.current_user = None
     return redirect(url_for('signin'))
+
+@app.route('/add_tweet', methods=['GET', 'POST'])
+def add_tweet():
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+        try:
+            tweet = {"title": title,"text": text}
+            db.child("Tweets").push(tweet)
+        except:
+            print("Couldn't add tweet")
+    return render_template("add_tweet.html")
+
+@app.route('/all_tweets')
+def all_tweets():
+    tweets=db.child("Tweets").get().val()
+    return render_template("all_tweets.html", tweets=tweets)
 
 
 if __name__ == '__main__':
